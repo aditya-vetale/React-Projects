6f198e7e4b8b9e66949c749853103532e77455d0
aditya-vetale <adityavetale7028@gmail.com>
2022-11-05 18:51:41 +0530

    Every Component completed

--- temperature_control/src/App.js
+++ temperature_control/src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [temperature, setTemperature] = useState(0);
+  const [temperatureColor, setTemperatureColor] = useState("cold");
+  function increment() {
+    setTemperature(temperature + 1);
+  }
+
+  function decrement() {
+    setTemperature(temperature - 1);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="app-container">
+      <div className="temperature-display-container">
+        <div
+          className={
+            temperature <= 10
+              ? "temperature-display cold"
+              : "temperature-display hot"
+          }
         >
-          Learn React
-        </a>
-      </header>
+          {temperature}
+        </div>
+      </div>
+      <div className="button-container">
+        <button onClick={increment}>+</button>
+        <button onClick={decrement}>-</button>
+      </div>
     </div>
   );
 }
